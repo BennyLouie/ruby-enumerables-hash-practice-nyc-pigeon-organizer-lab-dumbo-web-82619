5e7c6f9f302d68e335c35909446f38f339fbc7d4
Benny Louie <bennylouie1412@gmail.com>
2019-08-10 18:40:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,15 +5,15 @@ def nyc_pigeon_organizer(data)
   pigeon_names = []
   i = 0 
   pigeon_names = data[:gender].values[0] | data[:gender].values[1]
-  while i < pigeon_names.length do 
-    data.each do |data_key, value|
-      binding.pry
-      data_key.each do |key,value|
+  data.each do |data_key, value|
+    binding.pry
+    data_key.each do |key,value|
+      while i < pigeon_names.length do
         if key.values.include?(pigeon_names[i])
-          pigeon_list[pigeon_names[i]][data_key] = []
-          pigeon_list[pigeon_names[i]][data_key] = pigeon_list[pigeon_names[i]][data_key].push(key.to_s)
+         pigeon_list[pigeon_names[i]][data_key] = []
+         pigeon_list[pigeon_names[i]][data_key] = pigeon_list[pigeon_names[i]][data_key].push(key.to_s)
         end
-        i += 1 
+      i += 1 
       end
     end
   end
