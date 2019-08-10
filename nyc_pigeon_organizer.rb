455eab7c2f88d16c5238ac7ddac75ec1f98394d3
Benny Louie <bennylouie1412@gmail.com>
2019-08-10 20:09:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,7 @@ def nyc_pigeon_organizer(data)
     data_value.each do |key, value|
       value.each do |name|
         if pigeon_list.has_key?(name)
-          if pigeon_list[name].has_key?(name)
+          if pigeon_list[name].has_key?(data_key)
             pigeon_list[name][data_key].push(key.to_s)
           end
         elsif !pigeon_list.has_key?(name)
