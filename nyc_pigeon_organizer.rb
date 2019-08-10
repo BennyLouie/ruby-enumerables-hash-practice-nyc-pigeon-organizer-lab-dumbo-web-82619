b19329d5d0177e6a87047a338fe8ee9da24145b1
Benny Louie <bennylouie1412@gmail.com>
2019-08-10 18:14:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,5 +5,6 @@ def nyc_pigeon_organizer(data)
   pigeon_names = []
   pigeon_names = data[:gender].values[0] | data[:gender].values[1]
   data.each do |key, value|
-    
+    key.each do |key,value|
+      
 end
